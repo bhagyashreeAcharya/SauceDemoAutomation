35aca473b229fbba62c1b49a5f7a7f89f4864ab5
Bhagyashree Acharya <bhag195naid@gmail.com>
2026-02-18 20:11:25 +0530

    Initial Selenium TestNG project setup

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -2,20 +2,39 @@ package base;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+
+import java.time.Duration;
+import java.util.HashMap;
+import java.util.Map;
 
 public class BaseTest {
 
     protected WebDriver driver;
+    protected WebDriverWait wait;
 
     @BeforeMethod
     public void setup() {
 
-        driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+
+        Map<String, Object> prefs = new HashMap<>();
+        prefs.put("credentials_enable_service", false);
+        prefs.put("profile.password_manager_enabled", false);
+        prefs.put("profile.password_manager_leak_detection", false);
+
+        options.setExperimentalOption("prefs", prefs);
+
+        driver = new ChromeDriver(options);
+
+        driver.manage().window().maximize();
+
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
 
         driver.get("https://www.saucedemo.com/");
-
     }
 
     @AfterMethod
@@ -24,5 +43,4 @@ public class BaseTest {
         driver.quit();
 
     }
-
 }
